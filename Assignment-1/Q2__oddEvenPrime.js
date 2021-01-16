37fc031e4dd92f72a0499f1f47262354e78344d4
Adarsh Shivhare <adarshshivhare789@gmail.com>
2021-01-16 11:48:06 +0530

    odd Even Prime

---
+++ Assignment-1/Q2__oddEvenPrime.js
@@ -0,0 +1,34 @@
+let n = 5;
+
+for (let i = 0; i < n; i++) {
+  if (primeCheck(i)) {
+    if (i == 2) {
+      document.write(i + ' is even and prime <br>');
+    } else {
+      document.write(i + ' is odd and prime <br>');
+    }
+  } else if (i % 2 == 0) {
+    document.write(i + ' is even <br>');
+  } else {
+    document.write(i + ' is odd <br>');
+  }
+}
+
+function primeCheck(x) {
+  let isPrime = true;
+  for (let i = 2; i < x; i++) {
+    if (x % i == 0) {
+      isPrime = false;
+      break;
+    }
+  }
+  if (x == 0 || x == 1) {
+    return false;
+  } else {
+    if (isPrime) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
