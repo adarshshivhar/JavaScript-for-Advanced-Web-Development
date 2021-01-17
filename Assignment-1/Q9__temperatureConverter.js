f13acba3965802a295146e9ca7c11d589d0efb84
Adarsh Shivhare <adarshshivhare789@gmail.com>
2021-01-17 12:04:33 +0530

    Temperature Converter Comment related to question

--- Assignment-1/Q9__temperatureConverter.js
+++ Assignment-1/Q9__temperatureConverter.js
@@ -1,3 +1,12 @@
+/** The Temperature Converter
+Create a function called Celsius to Fahrenheit:
+● Store a Celsius temperature into a variable.
+● Convert it to Fahrenheit and output "NN°C is NN°F".
+Create a function called Fahrenheit to celsius:
+● Now store a Fahrenheit temperature into a variable.
+● Convert it to celsius and output "NN°F is NN°C."
+10.  */
+ 
 function celToFer(celsius) {
     var celTemp = celsius;
     var cToF = celTemp * 9 / 5 + 32;
