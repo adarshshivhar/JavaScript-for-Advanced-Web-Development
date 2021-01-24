2bac370d1842ffd8ec03659e4ab2e193186853c0
Adarsh Shivhare <adarshshivhare789@gmail.com>
2021-01-24 11:39:36 +0530

    Join Elements

---
+++ Assignment-2/Q2__joinElements.js
@@ -0,0 +1,9 @@
+let myColor = ['Red', 'Green', 'White', 'Black'];
+
+document.write(myColor.join(','));
+document.write('<br>');
+document.write('<br>');
+document.write(myColor.join('+'));
+document.write('<br>');
+document.write('<br>');
+document.write(myColor.join('-'));
