0344984ef4d27a5045a1f936f70fa7c8da824655
Adarsh Shivhare <adarshshivhare789@gmail.com>
2021-01-24 12:33:10 +0530

    Clock js file update

--- Assignment-2/Q1__clock.js
+++ Assignment-2/Q1__clock.js
@@ -1,7 +1,7 @@
 let hourFormat = 'twelve';
 
 function setFormat(hour) {
-    hourFormat = hour;
+  hourFormat = hour;
 }
 
 setInterval(showTime, 1000);
@@ -10,31 +10,25 @@ function showTime() {
   let hour = time.getHours();
   let min = time.getMinutes();
   let sec = time.getSeconds();
-   am_pm = 'AM';
+  am_pm = 'AM';
 
-   if (hourFormat === 'twelve') {
-     if (hour > 12) {
-       hour -= 12;
-       am_pm = 'PM';
-     }
-     if (hour == 0) {
-       hr = 12;
-       am_pm = 'AM';
-     }
+  if (hourFormat === 'twelve') {
+    if (hour > 12) {
+      hour -= 12;
+      am_pm = 'PM';
+    }
+    if (hour == 0) {
+      hr = 12;
+      am_pm = 'AM';
+    }
+  } else if (hourFormat === 'twentyfour') {
+    am_pm = '';
+  }
 
-     hour = hour < 10 ? '0' + hour : hour;
-     min = min < 10 ? '0' + min : min;
-     sec = sec < 10 ? '0' + sec : sec;
-
-   } else if (hourFormat === 'twentyfour') {
-     am_pm = '';
-     hour = hour < 10 ? '0' + hour : hour;
-     min = min < 10 ? '0' + min : min;
-     sec = sec < 10 ? '0' + sec : sec;
-   }
-
-   let currentTime = hour + ':' + min + ':' + sec + am_pm;
-   document.getElementById('clock').innerHTML = currentTime;
-    
+  hour = hour < 10 ? '0' + hour : hour;
+  min = min < 10 ? '0' + min : min;
+  sec = sec < 10 ? '0' + sec : sec;
+  let currentTime = hour + ':' + min + ':' + sec + am_pm;
+  document.getElementById('clock').innerHTML = currentTime;
 }
 showTime();
