6bccdbabb3c04c23c1b120e93a9cb9971a90c80b
Adarsh Shivhare <adarshshivhare789@gmail.com>
2021-01-24 13:00:17 +0530

    clock js file update

--- Assignment-2/Q1__clock.js
+++ Assignment-2/Q1__clock.js
@@ -10,9 +10,13 @@ function showTime() {
   let hour = time.getHours();
   let min = time.getMinutes();
   let sec = time.getSeconds();
-  am_pm = 'AM';
+  
 
   if (hourFormat === 'twelve') {
+    am_pm = 'AM';
+    if(hour == 12) {
+      am_pm = 'PM';
+    }
     if (hour > 12) {
       hour -= 12;
       am_pm = 'PM';
